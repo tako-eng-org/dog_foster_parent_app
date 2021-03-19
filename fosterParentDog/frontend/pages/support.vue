1ae81bc30670b8695debadd137474674c200f09e
tako_eng <oys.sys@gmail.com>
2021-03-19 11:54:18 +0900

    #8 Add style.css

---
+++ fosterParentDog/frontend/pages/support.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>support</h1>
+
+    <button type="submit"
+            class="btn btn-primary"
+            v-on:click="doEditConsole">
+      テストボタン
+    </button>
+
+  </div>
+</template>
+
+<script>
+// process.env.DEBUG = 'nuxt:*' // nuxt.jsについてログ出力する
+
+export default {
+  methods:{
+    doEditConsole() {
+      console.log("*****************support********************")
+    },
+  }
+}
+</script>
+
+<!-- cssはassetsから自動で読み込む-->
+<style></style>
