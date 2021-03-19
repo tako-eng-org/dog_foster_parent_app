f717571f3de2f1c960c784dff4c7bd1ee733c793
tako_eng <oys.sys@gmail.com>
2021-03-19 11:41:19 +0900

    #8 Update default.vue

--- fosterParentDog/frontend/layouts/default.vue
+++ fosterParentDog/frontend/layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-    <Nuxt />
+    <Header />
+    <main>
+      <nuxt />
+    </main>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue"; // headerをインポート
+import Footer from "@/components/Footer.vue"; // footerをインポート
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
 
 <style>
 html {
