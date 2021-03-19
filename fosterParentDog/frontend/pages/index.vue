1eb39fb037b7930973eafefe60495acb874aeb59
tako_eng <oys.sys@gmail.com>
2021-03-19 11:52:00 +0900

    #8 Update index.vue

--- fosterParentDog/frontend/pages/index.vue
+++ fosterParentDog/frontend/pages/index.vue
@@ -1,73 +1,92 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="container">
+      <h1>里親募集 掲示板</h1>
+    </div>
+
+    <div class="container">
+      <div class="one-contents">
+        <div class='col-xs-12'>
+          <img class='float-left'
+               style='padding:0;margin:0 15px 0 0;'
+               src="../assets/image/dog-sample1.png"
+               width="130"
+               height="130">
+        </div>
+        <p class="mb-0">No125 はなこ</p>
+        <p class="mb-0">犬種: 雑種</p>
+        <p class="mb-0">性別: メス</p>
+        <p class="mb-0">名前: はなこ</p>
+        <p class="mb-0">内容: 元気で可愛いお婆ちゃん犬です。 シニアではありますが、…</p>
+        <p class="mb-0">
+          <nuxt-link to="./detail">詳細をみる</nuxt-link>
+        </p>
       </div>
+    </div>
+    <br>
+
+    <div class="container">
+      <div class="one-contents">
+        <div class='col-xs-12'>
+          <img class='float-left'
+               style='padding:0;margin:0 15px 0 0;'
+               src="../assets/image/dog-sample2.png"
+               width="130"
+               height="130">
+        </div>
+        <p class="mb-0">No124 たろう</p>
+        <p class="mb-0">犬種: 和犬系の雑種</p>
+        <p class="mb-0">性別: オス</p>
+        <p class="mb-0">名前: たろう</p>
+        <p class="mb-0">内容: 元気な男の子です。 人に対してはさほど警戒せず、…</p>
+        <p class="mb-0">
+          <nuxt-link to="./detail">詳細をみる</nuxt-link>
+        </p>
+      </div>
+    </div>
+    <br>
+
+    <div class="container">
+      <p class="text-center"> << 1 2 3 4 5 >> </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+const axios = require('axios');
+process.env.DEBUG = 'nuxt:*' // nuxt.jsについてログ出力する
+console.log("test...............")
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<!-- cssはassetsから自動で読み込む-->
+<style></style>
+<!--<template>-->
+<!--  <div>-->
+<!--    <compo></compo>-->
+<!--  </div>-->
+<!--</template>-->
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+<!--<script>-->
+<!--process.env.DEBUG = 'nuxt:*' // nuxt.jsについてログ出力する-->
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+<!--import Compo from '../components/Top'-->
 
-.links {
-  padding-top: 15px;
-}
-</style>
+<!--export default {-->
+<!--  components: {-->
+<!--    compo: Compo,-->
+<!--  },-->
+<!--}-->
+<!--</script>-->
