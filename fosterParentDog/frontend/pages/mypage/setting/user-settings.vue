92639e60d7d8dace656ab28e25f8135f3af08028
tako_eng <oys.sys@gmail.com>
2021-03-19 11:55:12 +0900

    #8 Add user-settings.vue

---
+++ fosterParentDog/frontend/pages/mypage/setting/user-settings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>user-settings</h1>
+  </div>
+</template>
+
+<script>
+process.env.DEBUG = 'nuxt:*' // nuxt.jsについてログ出力する
+
+// export default {
+//   methods:{
+//     doLoginConsole() {
+//       console.log("******************login******************")
+//     },
+//   }
+// }
+</script>
+
+<!-- cssはassetsから自動で読み込む-->
+<style></style>
