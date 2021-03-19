62991e8b115cb32809da25d65d1d7cede2caa7cc
tako_eng <oys.sys@gmail.com>
2021-03-19 11:44:41 +0900

    #8 Update entry-edit.vue

---
+++ fosterParentDog/frontend/pages/mypage/entrie/entry-edit.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>entry-edit</h1>
+
+    <button type="submit"
+            class="btn btn-primary"
+            v-on:click="doEditConsole">
+      テストボタン
+    </button>
+  </div>
+</template>
+
+<script>
+// process.env.DEBUG = 'nuxt:*' // nuxt.jsについてログ出力する
+
+export default {
+  methods:{
+    doEditConsole() {
+      console.log("************************************")
+    },
+  }
+}
+</script>
+
+<!-- cssはassetsから自動で読み込む-->
+<style></style>
