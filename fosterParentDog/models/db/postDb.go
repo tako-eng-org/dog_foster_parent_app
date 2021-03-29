660dfc78670271213891ea7382ae7a68bd8c9a2b
tako_eng <oys.sys@gmail.com>
2021-03-29 16:13:51 +0900

    #9 Update postDb.go

--- fosterParentDog/models/db/postDb.go
+++ fosterParentDog/models/db/postDb.go
@@ -24,30 +24,50 @@ func FindAllRecords() []entity.Post {
 }
 
 //*******************************************************************
+// 公開済みレコードの数を取得する
+//*******************************************************************
+// return(ex): -> 41
+func CountPublishedPostNum() int64 {
+	table := []entity.Post{}
+	db := open()
+
+	var count int64
+
+	//SELECT count(id) FROM "post"  WHERE "post"."deleted_at" IS NULL AND ((publishing = '0'))
+	db.Model(&table).
+		Select("count(id)").
+		Where("publishing = ?", "0").
+		Count(&count)
+
+	defer db.Close() //defer 関数がreturnする時に実行される
+
+	return count
+}
+
+//*******************************************************************
 // 投稿を1ページ表示分取得する
 //*******************************************************************
 func FindIndexRecords(page string) []entity.Post {
-	allRecords := []entity.Post{}
+	records := []entity.Post{}
 	db := open()
 	// 数値に変換する
 	pageNum, _ := strconv.Atoi(page)
 
 	numberPerPage := 20 // 1ページあたりの表示件数
 
-	//select
+	//SELECT * FROM "post"  WHERE "post"."deleted_at" IS NULL ORDER BY id desc LIMIT 20 OFFSET 0
 	db.Order("id desc").
-		//Limit(20).Offset(0).
 		Limit(numberPerPage).
 		Offset((pageNum - 1) * numberPerPage).
-		Find(&allRecords)
+		Find(&records)
 
 	defer db.Close() //defer 関数がreturnする時に実行される
 
-	return allRecords
+	return records
 }
 
 //*******************************************************************
-// テーブルレコードをidをキーに全件取得する
+//
 //*******************************************************************
 func InsertRecord(registerRecord *entity.Post) {
 	db := open()
