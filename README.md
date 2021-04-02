548ccf7bd4d900df6953f235a367daa29b4d20ed
tako_eng <oys.sys@gmail.com>
2021-04-02 15:47:02 +0900

    #9 Update

--- README.md
+++ README.md
@@ -17,3 +17,26 @@ docker-compose up -d nginx
 docker-compose up -d pgadmin
 # access to http://localhost:8001
 ```
+
+
+編集中
+```
+# golandでDockerコンテナをアタッチしてデバッグしたいとき
+# port2345で待ち受けるのみ/サーバー(8090)を動かしたい場合は、golandの実行構成にあるのでデバッグボタンを押下すること。
+
+# .realize.ymlのrunをfalseにする
+
+# このスクリプトが実行されていることをログから確認したら、
+# -----remoteDebug or hotReload start-----
+# API server listening at: [::]:2345
+
+# golandのデバッグボタン押下してサーバを起動する
+# [GIN-debug] Listening and serving HTTP on :8090
+
+# curlを叩いてブレークポイントを貼ったところで止まればOK
+# curl -v localhost:8000/fosterparent/fetchAllRecords
+
+# 1度実行すると不安定になるので、下記コマンドでコンテナ再起動した方が早いこともある(再現しない)
+# docker-compose restart dog_app
+
+```
--- fosterParentDog/docker-entrypoint.sh
+++ fosterParentDog/docker-entrypoint.sh
@@ -14,28 +14,7 @@ export GO111MODULE=on #go.modをgo build時に実行するための環境変数�
 export GOPATH=''
 
 echo "-----remoteDebug or hotReload start-----"
-# golandでDockerコンテナをアタッチしてデバッグしたいとき
-# port2345で待ち受けるのみ/サーバー(8090)を動かしたい場合は、golandの実行構成にあるのでデバッグボタンを押下すること。
-
-# .realize.ymlのrunをfalseにする
-
-# このスクリプトが実行されていることをログから確認したら、
-# -----remoteDebug or hotReload start-----
-# API server listening at: [::]:2345
-
-# golandのデバッグボタン押下してサーバを起動する
-# [GIN-debug] Listening and serving HTTP on :8090
-
-# curlを叩いてブレークポイントを貼ったところで止まればOK
-# curl -v localhost:8000/fosterparent/fetchAllRecords
-
-# 1度実行すると不安定になるので、下記コマンドでコンテナ再起動した方が早いこともある(再現しない)
-# docker-compose restart dog_app
-
 #dlv debug --headless --listen=:2345 --api-version=2 --accept-multiclient
-# ホットリロードでコンテナ起動したいとき
-# realize.ymlにそってホットリロードを実行する
 /go/bin/realize start;
-echo "!!!!!!!!!exit!!!!!!!!!"
-sleep 5000;
 echo "-----remoteDebug or hotReload end-----"
+sleep 5000;
