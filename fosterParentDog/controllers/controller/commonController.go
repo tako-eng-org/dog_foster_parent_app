56fd1f7653d1488d1b0d08401c112b378e425d76
tako_eng <oys.sys@gmail.com>
2021-04-02 15:36:34 +0900

    #9 Update

--- fosterParentDog/controllers/controller/commonController.go
+++ fosterParentDog/controllers/controller/commonController.go
@@ -1,26 +1,9 @@
 package controller
 
-import (
-	// 文字列と基本データ型の変換パッケージ
-	//strconv "strconv"
-
-	// Gin
-	"github.com/gin-gonic/gin"
-	//"strconv"
-
-	//"fpdapp/models/entity"
-
-	// エンティティ(データベースのテーブルの行に対応)
-	//entity "fpdapp/models/entity"
-
-	// DBアクセス用モジュール
-	db "fpdapp/models/db"
-)
-
 // 全ての投稿情報を取得する
-func FetchAllRecords(c *gin.Context) {
-	result := db.FindAllRecords()
-
-	// URLへのアクセスに対してJSONを返す
-	c.JSON(200, result)
-}
+//func FetchAllRecords(c *gin.Context) {
+//	result := db.FindAllRecords()
+//
+//	// URLへのアクセスに対してJSONを返す
+//	c.JSON(200, result)
+//}
