c7683ad0069fef6c59ba0e810e428b535a0d3f0a
tako_eng <oys.sys@gmail.com>
2021-04-06 21:06:15 +0900

    #9 Update Dockerfile docker-entrypoint.sh パッケージインストールをDockerfileで実行するよう修正した。（イメージビルド時のみ実行で良いコマンドのため）

--- fosterParentDog/docker-entrypoint.sh
+++ fosterParentDog/docker-entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 echo "-----`basename $0`-----"
 
-# エラー対策
-# cgo: exec gcc: exec: "gcc": executable file not found in $PATH
-export CGO_ENABLED=0
-
-echo "-----dlv realize install start-----"
-GO111MODULE=off go get -u github.com/derekparker/delve/cmd/dlv;
-GO111MODULE=off go get -u github.com/oxequa/realize;
-echo "-----dlv realize installed-----"
-
 export GO111MODULE=on #go.modをgo build時に実行するための環境変数設定
 export GOPATH=''
 
@@ -18,3 +9,4 @@ echo "-----remoteDebug or hotReload start-----"
 /go/bin/realize start;
 echo "-----remoteDebug or hotReload end-----"
 sleep 5000;
+exit 1
