b046b69ab3642e27fc4621b6f77c553de1d5bcc2
tako_eng <oys.sys@gmail.com>
2021-04-09 19:44:20 +0900

    #18 Update docker-entrypoint.sh go env設定を追加した（dlvに影響する）

--- fosterParentDog/docker-entrypoint.sh
+++ fosterParentDog/docker-entrypoint.sh
@@ -3,6 +3,7 @@ echo "-----`basename $0`-----"
 
 export GO111MODULE=on #go.modをgo build時に実行するための環境変数設定
 export GOPATH=''
+export CGO_ENABLED=0
 
 echo "-----remoteDebug or hotReload start-----"
 #dlv debug --headless --listen=:2345 --api-version=2 --accept-multiclient
