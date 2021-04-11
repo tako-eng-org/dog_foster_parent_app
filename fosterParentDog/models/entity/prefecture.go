536bc5e5e35ebf2d47ac3455f81dbbb32d1fcfdd
tako_eng <oys.sys@gmail.com>
2021-04-11 14:40:28 +0900

    #18 都道府県マスター定義に読み仮名を追加

--- fosterParentDog/models/entity/prefecture.go
+++ fosterParentDog/models/entity/prefecture.go
@@ -4,5 +4,6 @@ package entity
 //47都道府県一覧を保存しておくマスターテーブルである。
 type Prefecture struct {
 	Id   uint   `gorm:"primary_key							"`                //都道府県ID
-	name string `gorm:"type:varchar(10);	unique	not null"` //都道府県名
+	Name string `gorm:"type:varchar(10);	unique	not null"` //都道府県名
+	Kana string `gorm:"type:varchar(20);	unique	not null"` //都道府県読み仮名
 }
