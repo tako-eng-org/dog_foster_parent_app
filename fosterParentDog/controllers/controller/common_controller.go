ca8301e7033b5dce7d70e9b6281a5e9fba32c70b
tako_eng <oys.sys@gmail.com>
2021-04-15 17:54:57 +0900

    #18 共通関数をdb->controllerに移動

--- fosterParentDog/controllers/controller/common_controller.go
+++ fosterParentDog/controllers/controller/common_controller.go
@@ -7,3 +7,20 @@ type (
 		DbConn *db.Database
 	}
 )
+
+//*******************************************************************
+// 型変換する
+//*******************************************************************
+func ToInt(i interface{}) int {
+	var r, _ = i.(int)
+	return r
+}
+func ToStr(i interface{}) string {
+	var r, _ = i.(string)
+	return r
+}
+
+func ToInt64(i interface{}) uint64 {
+	var r, _ = i.(uint64)
+	return r
+}
