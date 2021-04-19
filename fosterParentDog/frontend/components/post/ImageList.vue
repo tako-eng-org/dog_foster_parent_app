e334b9f9751e79a49197154989798e4b3e188aae
tako_eng <oys.sys@gmail.com>
2021-04-19 22:00:35 +0900

    #18 イメージリストコンポーネントのファイル名修正

--- fosterParentDog/frontend/components/post/ImageList.vue
+++ fosterParentDog/frontend/components/post/ImageList.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="image-list">
     <div class="container" v-for="imagePath in imagePathList" v-bind:key="imagePath.post_id">
-      <ImageComponent :imagepath="imagePath.image_path"/>
+      <ImageOne :imagePath="imagePath.image_path"/>
     </div>
   </div>
 </template>
 
 <script>
+import ImageOne from "~/components/post/Image";
+
 export default {
+  data() {
+    return {
+      isView: true,
+    }
+  },
   props: {
     imagePathList: Object,
+  },
+  components: {
+    ImageOne,
   },
 }
 </script>
