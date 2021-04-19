dd094e7871fb9e74f1f65b02e512ea4982699075
tako_eng <oys.sys@gmail.com>
2021-04-19 22:05:22 +0900

    #18 共通化後のコンポーネントを追加(個別のコンポーネントを廃止した)

---
+++ fosterParentDog/frontend/components/post/ReadOnly.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="component">
+    <div class="row">
+      <p v-if="isView">{{ title }} : {{ detail }}</p>
+      <!--      elseルートなし-->
+      <!--      <p v-else>{{ itemTitle }} <input v-model="itemName" placeholder=""></p>-->
+      <!--      <p>投稿日時 : {{ createdAt }}</p>-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isView: true,
+    }
+  },
+  props: {
+    title: String,
+    detail: String,
+  },
+}
+</script>
---
+++ fosterParentDog/frontend/components/post/TextOrTextBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <div class="row">
+      <p v-if="isView">{{ title }} : {{ detail }}</p>
+      <p v-else>{{ title }} <input v-model="name" placeholder=""></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isView: true,
+    }
+  },
+  props: {
+    title: String,
+    detail: String,
+  },
+}
+</script>
