cfd88cd0e0fac1743aced863b34274fcd7c91955
tako_eng <oys.sys@gmail.com>
2021-04-24 19:16:50 +0900

    #18 不要ファイルを削除

--- fosterParentDog/frontend/consts/getLabel.js
+++
@@ -1,8 +0,0 @@
-let getLabel = (mapName, i) => {
-  return mapName.find(map => map.value === i).label
-}
-
-export default getLabel;
-// export default ({}, inject) => {
-//   inject('getLabel', getLabel);
-// }
