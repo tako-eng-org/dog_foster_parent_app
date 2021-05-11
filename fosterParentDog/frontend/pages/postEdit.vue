d042f05ac6a42a56f3e79910c0bf35c8622b4436
tako_eng <oys.sys@gmail.com>
2021-05-11 23:25:45 +0900

    #16 投稿機能について、画像処理をTODOに変更

--- fosterParentDog/frontend/pages/postEdit.vue
+++ fosterParentDog/frontend/pages/postEdit.vue
@@ -2,10 +2,6 @@
   <!-- TODO 新規追加,更新の順に作成する -->
   <div class="post-edit">
     <div class="container">
-      <!--   **********debug**********   -->
-      <button type="submit" class="btn btn-primary" @click="testMethod">console</button>
-      <!--   **********debug**********   -->
-
       <TextBox :title="'犬の名前'" v-model="post.dogName" :readonly="false"/>
       <p class="text-primary">debug_犬の名前:{{ post.dogName }}</p>
 
@@ -36,9 +32,6 @@
       <TextBox :title="'アピールポイント'" v-model="post.appealPoint" :readonly="false"/>
       <p class="text-primary">debug_アピールポイント:{{ post.appealPoint }}</p>
 
-      <!--   TODO: 譲渡可能都道府県のCreate実装   -->
-      <!--      <PostPrefecture v-model="post.postPrefectureList.post_prefecture_id" :readonly="false"/>-->
-      <!--      <p class="text-primary">debug_譲渡可能都道府県:{{ post.postPrefectureList.post_prefecture_id }}</p>-->
       <PostPrefecture v-model="post.postPrefectureIdList" :readonly="false"/>
       <p class="text-primary">debug_譲渡可能都道府県:{{ post.postPrefectureIdList }}</p>
 
@@ -46,21 +39,19 @@
       <p class="text-primary">debug_その他特記事項:{{ post.otherMessage }}</p>
 
       <hr>
-      <!--   TODO トップ画像をアップロードする   -->
-      <p>トップ画像をアップロードしてください(サムネイルや目立つ箇所に表示されます)</p>
-      <UploadOne v-model="post.topImagePath"/>
-      <p class="text-primary">debug_picture is: {{ post.topImagePath }}</p>
+      <!-- 画像アップロード -->
+      <!--   TODO: 画像の投稿機能は後日再度実装予定。ファイルをアップロードし、ファイル名で表示する。post_idを直指定。プレビュー機能はなし-->
 
-      <!--   TODO サブ画像をアップロードする(任意,9枚まで)uploadOne使うか？   -->
+      <!--      <div class="upload-top-image">-->
+      <!--        <input type="file" id="top-image" ref="input">-->
+      <!--        <button type="submit" @click="ImageUpload(0)">トップ画像をアップロードする</button>-->
+      <!--        <ImageOne :imagePath="getTopImagePath()"/>-->
+      <!--      </div>-->
 
       <!--  投稿ボタン  -->
-      <button type="submit" class="btn btn-primary" @click="postCreate">投稿する</button>
-
-      <!--  debug_画像ボタン  -->
-      <button type="submit" class="btn btn-primary" @click="imageCreate">画像投稿デバッグ用</button>
-
+      <button type="submit" class="btn btn-primary" @click="postCreate(1)">投稿する</button>
       <!--  下書きに保存ボタン  -->
-      <!--          <button type="submit" class="btn btn-primary" @click="postCreate">下書きに保存</button>-->
+      <button type="submit" class="btn btn-primary" @click="postCreate(0)">下書きに保存する</button>
     </div>
 
   </div>
@@ -98,9 +89,11 @@ export default {
   },
   data() {
     return {
+      picture: null,
+
       post: {
         //基礎投稿
-        // id: 投稿時に自動付与するため、新規作成では無し
+        // id: 投稿時に自動付与するため、新規作成では無し。update実装時に更新予定
         // created_at:
         // updated_at:
         publishing: 1,
@@ -115,34 +108,41 @@ export default {
         introduction: "test_introduction",
         appealPoint: "test_appeal_point",
         otherMessage: "test_other_message",
-        topImagePath: "test_top_image_path",
-
-        imagePathList: [], //画像パスリスト
-
+        user_id: 1, // TODO: ログインユーザーのIDが入る(入れないとindex表示時にnullエラーになりdog_app再起動が必要になるためdebug用として1を入れている)
+        postImages: [ //画像パスリスト
+          {
+            position: 0,
+            objectKey: "images/1_767121c4-4b8b-4874-9690-008e0fcd3fae.png",
+            objectUrl: "https://bbsapp-img.s3.us-east-2.amazonaws.com/images/000002.png",
+          },
+        ],
         postPrefectureIdList: [], //譲渡可能都道府県リスト
-
-        // user: { //ユーザー情報
-        //   user_id: 1,//debug
-        // },
       },
+
+      topObjectUrl: "", //https://bbsapp-img.s3.us-east-2.amazonaws.com/images/1_8a217b9f-b0de-441f-b68d-861a7cd5f4be.png
+      topObjectKey: "", //images/1_8a217b9f-b0de-441f-b68d-861a7cd5f4be.png
     }
   },
   computed: {},
   methods: {
     /**
-     * debug用
+     * 1投稿のpositionが最小値(原則0)のimagePathを取得する
+     * @param {object} post
      */
-    testMethod() {
-      console.log("-------testMethod");
-      // console.log(this.postData);
+    getTopImagePath() {
+      let min = Math.min(...this.post.postImages.map(x => x.position));
+      let ImagePathByMinPosition = this.post.postImages.filter(e => (e.position === min))[0].image_path;
+      console.log("ImagePathByMinPosition-------");
+      console.log(ImagePathByMinPosition);
+      return ImagePathByMinPosition
     },
 
     /**
      * 投稿記事を追加する
      */
-    postCreate() {
+    postCreate(publishing) {
       let postJson = {
-        publishing: this.post.publishing,
+        publishing: publishing,
         dog_name: this.post.dogName,
         breed: this.post.breed,
         gender: this.post.gender,
@@ -154,8 +154,8 @@ export default {
         introduction: this.post.introduction,
         appeal_point: this.post.appealPoint,
         other_message: this.post.otherMessage,
-        user_id: 1, // TODO: ログインユーザーのIDが入る(入れないとindex表示時にnullエラーになりdog_app再起動が必要になるためdebug用として1を入れている)
-        top_image_path: this.post.topImagePath,
+        user_id: this.user_id,
+        top_image_path: this.topObjectKey,
         image_path_list: [
           "12345",
           "6789",
@@ -173,27 +173,64 @@ export default {
           } else {
             console.log(`投稿完了:${response.status}:${response.statusText}, ${this.postCreate.name}`);
           }
-        }).catch(err => console.error(err.response));
+        })
+        .catch(err => console.error(err.response));
     },
 
     /**
-     * top画像を投稿する（debug用）
+     * debug用
      */
-    imageCreate() {
-      let postJson = {
-        top_image_path: this.post.topImagePath,
-      }
-      console.log("--------postJSON");
-      console.log(postJson);
-      this.$axios.post('/api/image_create', postJson)
-        .then((response) => {
-          if ((response.status !== 201)) {
-            console.error("error-201以外");
-            console.error(`画像エラー:${response.status}:${response.statusText}, ${this.imageCreate.name}`)
-          } else {
-            console.log(`画像完了:${response.status}:${response.statusText}, ${this.imageCreate.name}`);
+    ImageUpload(position) {
+      let file = this.$refs.input;
+      let formData = new FormData();
+      formData.append('image', file.files[0]);
+      formData.append('user_id', this.post.user_id);
+      formData.append('position', position);
+      console.log(...formData.entries());
+      return new Promise((resolve, reject) => {
+        this.$axios.post('/api/image_upload', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
           }
-        }).catch(err => console.error(err.response));
+        )
+          .then((response) => {
+            if ((response.status !== 201)) {
+              console.error(`画像アップロードエラー:${response.status} ${response.statusText}`)
+            } else {
+              console.log(`画像アップロード完了:${response.status} ${response.statusText}`);
+              // this.topObjectUrl = response.data.object_url;
+              // this.topObjectKey = response.data.object_key;
+              // console.log(this.topObjectUrl);
+              // console.log(this.topObjectKey);
+              let imageArr = {
+                position: response.data.position,
+                objectKey: response.data.object_key,
+                objectUrl: response.data.object_url,
+              };
+              console.log("imageArr is --------");
+              console.log(imageArr);
+              this.post.postImages.push(imageArr);
+              console.log("this.post.postImages are --------");
+              console.log(this.post.postImages);
+            }
+          })
+          .catch(function (error) {
+            if (error.response) {
+              // 要求がなされたとサーバがステータスコードで応答した// 2XXの範囲外
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // 要求がなされたが、応答が受信されなかった
+              console.log(error.request);
+            } else {
+              // トリガーしたリクエストの設定に何かしらのエラーがある
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          })
+      });
     },
 
   },
@@ -201,4 +238,5 @@ export default {
 </script>
 
 <!-- cssはassetsから自動で読み込む-->
-<style scoped></style>
+<style scoped>
+</style>
