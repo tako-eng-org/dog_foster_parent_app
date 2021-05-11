61a377528533bff6a753991f04a52e1e07120c02
tako_eng <oys.sys@gmail.com>
2021-05-11 22:58:02 +0900

    #16 値変換処理を追加

--- fosterParentDog/controllers/controller/common_controller.go
+++ fosterParentDog/controllers/controller/common_controller.go
@@ -27,3 +27,20 @@ func strToUint64(i string) uint64 {
 	}
 	return r
 }
+
+func intToFloat64(i []int) []float64 {
+	f := make([]float64, len(i))
+	for n := range i {
+		f[n] = float64(i[n])
+	}
+	return f
+}
+
+func strToIntSlice(i []string) []int {
+	var f []int
+	for _, foo := range i {
+		j, _ := strconv.Atoi(foo)
+		f = append(f, j)
+	}
+	return f
+}
