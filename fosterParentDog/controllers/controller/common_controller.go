3e714cd6b4d77e063a3ea64f23add1bf6222b341
tako_eng <oys.sys@gmail.com>
2021-05-11 22:58:29 +0900

    #16 不要な関数を削除

--- fosterParentDog/controllers/controller/common_controller.go
+++ fosterParentDog/controllers/controller/common_controller.go
@@ -19,28 +19,3 @@ func strToInt(i string) int {
 	}
 	return r
 }
-
-func strToUint64(i string) uint64 {
-	var r, err = strconv.ParseUint(i, 10, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return r
-}
-
-func intToFloat64(i []int) []float64 {
-	f := make([]float64, len(i))
-	for n := range i {
-		f[n] = float64(i[n])
-	}
-	return f
-}
-
-func strToIntSlice(i []string) []int {
-	var f []int
-	for _, foo := range i {
-		j, _ := strconv.Atoi(foo)
-		f = append(f, j)
-	}
-	return f
-}
