b7e99d9b3a85009c9f4ca046d6bae31d3d0a0d16
tako_eng <oys.sys@gmail.com>
2021-05-19 19:45:55 +0900

    #11 ログインにまつわるテーブル定義を追加

--- fosterParentDog/models/entity/user.go
+++ fosterParentDog/models/entity/user.go
@@ -1,16 +1,21 @@
 package entity
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
 
-//ユーザーテーブル用
-//登録ユーザーのテーブルである。
+	"github.com/jinzhu/gorm"
+)
+
+//登録ユーザーのテーブル用
 type User struct {
-	gorm.Model            //ID, CreatedAt, UpdatedAt, DeletedAtを自動で定義する
-	Name           string `gorm:"type:varchar(20); unique not null"`  //ユーザー名
-	Email          string `gorm:"type:varchar(256);	unique not null"` //メールアドレス
-	Password       string `gorm:"type:varchar(128);	not null"`        //パスワード
-	TwitterAccount string `gorm:"type:varchar(20); unique"`           //twitterアカウント
-	Nickname       string `gorm:"type:varchar(30);"`                  //ニックネーム
-	Profile        string `gorm:"type:varchar(400);"`                 //自己紹介文
-	WebUrl         string `gorm:"type:varchar(100);"`                 //websiteやSNSなどのURL
+	gorm.Model                 //ID, CreatedAt, UpdatedAt, DeletedAtを自動で定義する
+	Name           string      `gorm:"type:varchar(20); unique not null"`  //ユーザー名
+	Email          string      `gorm:"type:varchar(256);	unique not null"` //メールアドレス
+	Password       string      `gorm:"type:varchar(128);	not null"`        //パスワード
+	TwitterAccount string      `gorm:"type:varchar(20); unique"`           //twitterアカウント
+	Nickname       string      `gorm:"type:varchar(30);"`                  //ニックネーム
+	Profile        string      `gorm:"type:varchar(400);"`                 //自己紹介文
+	WebUrl         string      `gorm:"type:varchar(100);"`                 //websiteやSNSなどのURL
+	SessionId      interface{} `gorm:"type:varchar(1000)"`                 //セッションID
+	LastLoginAt    time.Time   `gorm:"type:varchar(1000)"`                 //最終ログイン日時
 }
