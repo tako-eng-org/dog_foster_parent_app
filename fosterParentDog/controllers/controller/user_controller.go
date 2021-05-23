2a53cdde11bf5c7d3e2800fe8fbdd7f89c04e9d2
tako_eng <oys.sys@gmail.com>
2021-05-23 19:40:29 +0900

    #11 login処理を修正

--- fosterParentDog/controllers/controller/user_controller.go
+++ fosterParentDog/controllers/controller/user_controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"fpdapp/models/entity"
 	"log"
 	"net/http"
@@ -18,76 +17,61 @@ func (cont *Controller) GetUserMenu(c *gin.Context) {
 }
 
 func (cont *Controller) PostLogin(c *gin.Context) {
-	log.Println("------------main logic")
-	cookieUserName, _ := c.Cookie("user_name")
-	cookiePassword, _ := c.Cookie("password")
 	cookieSessionId, _ := c.Cookie("session-id")
-	//formUserName := c.PostForm("user_name")
-	//formPassword := c.PostForm("password")
+	formUserName := c.PostForm("user_name")
+	formPassword := c.PostForm("password")
 	var dbUser entity.User
-	userCount, dbUser := cont.DbConn.UserCount(cookieUserName)
-	fmt.Println("--------usercount ", userCount)
+	userCount, dbUser := cont.DbConn.UserCount(formUserName)
 
+	// 注意：ユーザ名がDBに存在しない場合&パスワード不一致の場合、同じエラーメッセージで返すこと。（ブルートフォース攻撃対策）
 	// フォームユーザ名がDBに存在しない場合、ログインさせない
-	if userCount == 0 { // ユーザ名がDBに存在しない場合、エラー
-		c.JSON(http.StatusBadRequest, gin.H{"err": "ユーザが存在しません。登録してください。"})
+	if userCount == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"err": "ユーザかパスワードが存在しません。登録してください。"})
 		c.Abort()
-	} else { // ユーザ名が存在する場合、次の処理へ
-		log.Println("ユーザが存在します。ログイン処理を続行します")
 	}
 
-	// formパスワードとDBパスワードが一致した場合、ログイン処理継続
-	if err := compareHashAndPassword(dbUser.Password, cookiePassword); err != nil {
+	// formパスワードとDBパスワードが不一致の場合、ログインさせない
+	if err := compareHashAndPassword(dbUser.Password, formPassword); err != nil {
 		log.Println("パスワード不一致：", err)
-		c.JSON(http.StatusBadRequest, gin.H{"err": err})
+		c.JSON(http.StatusBadRequest, gin.H{"err": "ユーザかパスワードが存在しません。登録してください。"})
 		c.Abort()
-	} else {
-		log.Println("パスワード一致。ログイン処理を続行します")
 	}
 
-	// ユーザ名が存在し、かつパスワードチェックもOKの場合、セッションIDとセッションユーザ名を発行
+	// ログイン成功した場合は、セッションIDとセッションユーザ名を発行
 	session := sessions.Default(c)
-	var user entity.User
-	fmt.Println("session-id is ", session.Get("session-id"))
 
-	//user.SessionId = session.Get("session-id" //こうするべき？でもsession-id入ってない
-	user.Name = cookieUserName
-	user.SessionId = cookieSessionId
-	user = cont.DbConn.UpdateSessionID(&user)
+	dbUser.SessionId = cookieSessionId
+	dbUser = cont.DbConn.UpdateSessionID(&dbUser)
 
+	log.Println("user_name is ", dbUser.Name)
 	session.Set("user_name", dbUser.Name)
 	session.Save()
-	//}
+
 	c.JSON(http.StatusOK, gin.H{"user name is*": session.Get("user_name")})
 }
 
-func (cont *Controller) BeforeAfter() gin.HandlerFunc {
+func (cont *Controller) SessionCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.Println("------------before logic")
 		session := sessions.Default(c)
-		fmt.Println(session)
+		log.Println("---session:", session)
 		cookieUserName, _ := c.Cookie("user_name")
 		cookieSessionId, _ := c.Cookie("session-id")
-		fmt.Println("cookieSessionId is", cookieSessionId)
 		var dbUser entity.User
 		_, dbUser = cont.DbConn.UserCount(cookieUserName)
 
 		if dbUser.SessionId == cookieSessionId &&
 			dbUser.Name == cookieUserName {
+			// セッション一致。ログイン不要
 			log.Println("DBのセッションID&ユーザ名と、cookieの情報が一致しました。すでにログイン済です。")
-			c.Set("session-id", cookieSessionId) //これいらない？
+			c.Set("session-id", cookieSessionId)
 			c.Set("user-name", cookieUserName)
 			// ユーザーの最終ログイン日時を更新する
 			dbUser = entity.User{Name: cookieUserName, LastLoginAt: time.Now()}
 			cont.DbConn.UpdateLastLoginAt(&dbUser)
-			c.Next() // 以降、関数実行後の後処理
-			log.Println("------------after logic2")
-			c.Abort()
 		} else {
+			// セッションなし。ログインが必要
 			log.Println("DBのセッションID&ユーザ名と、cookieの情報が一致しません。ログインしてください。")
-			// TODO: ログイン画面へ遷移
-			c.Next() // 以降、関数実行後の後処理
-			log.Println("------------after logic1")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "ログインしてください。"})
 			c.Abort()
 		}
 		log.Println("ログインチェック終わり")
--- fosterParentDog/server.go
+++ fosterParentDog/server.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
-
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -38,19 +37,18 @@ func serve() {
 	router.GET("/post", cont.FetchPost)
 
 	// 投稿レコード情報をDBへ登録する
-	router.POST("/post_create", cont.Create)
+	router.POST("/post_create", cont.SessionCheck(), cont.Create)
 
 	// 画像ファイルをS3へアップロードする
-	router.POST("/image_upload", cont.ImageUpload)
+	router.POST("/image_upload", cont.SessionCheck(), cont.ImageUpload)
 
 	// セッションの設定
 	store := cookie.NewStore([]byte("secret"))
 	router.Use(sessions.Sessions("session-id", store))
-	//router.Use(cont.BeforeAfter())
 
 	// 第2引数をミドルウェアとして関数の処理前後に実行する
-	router.POST("/login", cont.BeforeAfter(), cont.PostLogin)
-	router.POST("/logout", cont.BeforeAfter(), cont.PostLogout)
+	router.POST("/login", cont.PostLogin)
+	router.POST("/logout", cont.SessionCheck(), cont.PostLogout)
 
 	router.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"message": "ルートがない場合に表示するメッセージです"})
