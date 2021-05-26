c92893f48e09bd5113141b8f2a9374f3f17b09de
tako_eng <oys.sys@gmail.com>
2021-05-26 18:42:25 +0900

    #11 テーブル定義の不要箇所を削除

--- fosterParentDog/models/entity/user.go
+++ fosterParentDog/models/entity/user.go
@@ -8,14 +8,13 @@ import (
 
 //登録ユーザーのテーブル用
 type User struct {
-	gorm.Model            //ID, CreatedAt, UpdatedAt, DeletedAtを自動で定義する
-	Name           string `gorm:"type:varchar(20); unique not null"`  //ユーザー名
-	Email          string `gorm:"type:varchar(256);	unique not null"` //メールアドレス
-	Password       string `gorm:"type:varchar(128);	not null"`        //パスワード
-	TwitterAccount string `gorm:"type:varchar(20); unique"`           //twitterアカウント
-	Nickname       string `gorm:"type:varchar(30);"`                  //ニックネーム
-	Profile        string `gorm:"type:varchar(400);"`                 //自己紹介文
-	WebUrl         string `gorm:"type:varchar(100);"`                 //websiteやSNSなどのURL
-	//SessionId      interface{} `gorm:"type:varchar(1000)"`                 //セッションID
-	LastLoginAt time.Time `gorm:"type:varchar(1000)"` //最終ログイン日時
+	gorm.Model               //ID, CreatedAt, UpdatedAt, DeletedAtを自動で定義する
+	Name           string    `gorm:"type:varchar(20); unique not null"`  //ユーザー名
+	Email          string    `gorm:"type:varchar(256);	unique not null"` //メールアドレス
+	Password       string    `gorm:"type:varchar(128);	not null"`        //パスワード
+	TwitterAccount string    `gorm:"type:varchar(20); unique"`           //twitterアカウント
+	Nickname       string    `gorm:"type:varchar(30);"`                  //ニックネーム
+	Profile        string    `gorm:"type:varchar(400);"`                 //自己紹介文
+	WebUrl         string    `gorm:"type:varchar(100);"`                 //websiteやSNSなどのURL
+	LastLoginAt    time.Time `gorm:"type:datetime(6)"`                   //最終ログイン日時
 }
